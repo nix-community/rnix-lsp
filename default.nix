a6a87cabac354f235c256ee9083b3912e586705a
jD91mZM2 <me@krake.one>
2019-03-25 18:41:26 +0100

    Properly fix default.nix!
    See https://github.com/mozilla/nixpkgs-mozilla/issues/21#issuecomment-292722953

--- default.nix
+++ default.nix
@@ -3,13 +3,13 @@ let
   nixpkgs = import <nixpkgs> { overlays = [ overlay ]; };
   nightly = nixpkgs.latest.rustChannels.nightly;
   buildRustPackage = nixpkgs.rustPlatform.buildRustPackage.override {
-    rust = nightly;
+    rust = {
+      rustc = nightly.rust;
+      cargo = nightly.cargo;
+    };
   };
 in buildRustPackage {
   name = "nix-lsp";
   src = builtins.filterSource (path: _type: path != (toString ./target)) ./.;
   cargoSha256 = "1n01dzm0ngy1kn42xdmkcc83cxkk9552l5spd5a4ack4s4rdlrm2";
-
-  # See https://github.com/NixOS/nixpkgs/issues/25863#issuecomment-302633494
-  RUSTFLAGS="-L ${nightly.rust}/lib/rustlib/x86_64-unknown-linux-gnu/lib/";
 }
