7002927a0ec87e583bd687601dc385ee312a9d41
Andreas Fuchs <asf@boinkor.net>
2020-11-16 23:57:01 -0500

    Use more modern flake definition based on updated naersk
    This eliminates the `edition` keyword, and lets naersk do all the work
    that the previous flake definition used to pull in several other repos
    in for (NUR and a utility repo mainly).

--- flake.nix
+++ flake.nix
@@ -1,49 +1,23 @@
 {
   description = "A work-in-progress language server for Nix, with syntax checking and basic completion";
 
-  edition = 201909;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.naersk.url = "github:nmattia/naersk";
+  inputs.utils.url = "github:numtide/flake-utils";
 
-  inputs.naersk = {
-    url   = "github:nmattia/naersk";
-    flake = false;
-  };
-  inputs.mozilla = {
-    url   = "github:mozilla/nixpkgs-mozilla";
-    flake = false;
-  };
-  inputs.jd91mzm2 = {
-    url = "git+https://gitlab.com/jD91mZM2/nur-packages";
-  };
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
 
-  outputs = { self, nixpkgs, mozilla, naersk, jd91mzm2 }: let
-    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" ];
-  in
-    rec {
-      # Main package
-      packages = forAllSystems (system: {
-        rnix-lsp = let
-          mozillaBuilt = nixpkgs.legacyPackages."${system}".callPackage "${mozilla}/package-set.nix" {};
-          naerskBuilt  = nixpkgs.legacyPackages."${system}".callPackage naersk {};
+        defaultPackage = naersk-lib.buildPackage ./.;
 
-          rust = mozillaBuilt.latest.rustChannels.stable.rust;
-        in naerskBuilt.buildPackage {
-          name = "rnix-lsp";
-          src  = jd91mzm2.lib.cleanSourceRust ./.;
-          root = ./.;
-
-          cargo = rust.cargo;
-          rustc = rust.rustc;
+        defaultApp = {
+          type = "app";
+          program = "${self.defaultPackage."${system}"}/bin/rnix-lsp";
         };
       });
-      defaultPackage = forAllSystems (system: packages."${system}".rnix-lsp);
-
-      # Make it runnable with `nix app`
-      apps = forAllSystems (system: {
-        rnix-lsp = {
-          type    = "app";
-          program = "${self.packages."${system}".rnix-lsp}/bin/rnix-lsp";
-        };
-      });
-      defaultApp = forAllSystems (system: apps."${system}".rnix-lsp);
-    };
 }
