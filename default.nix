513695bac753a4695d3368e181251c3f4db6bed5
Andreas Fuchs <asf@boinkor.net>
2020-11-19 09:49:49 -0500

    Update niv-pinned packages as well
    * Use newer naersk, simplifying all of default.nix
    
    * Use the same nixpkgs channel as the flake (-unstable).
    
    * Drop mozilla, as it results in impure builds.
    
    * Drop NUR, used for a rust source code helper that's made unnecessary
      by newer naersk.

--- default.nix
+++ default.nix
@@ -2,15 +2,5 @@
   sources ? import ./nix/sources.nix,
   pkgs ? import sources.nixpkgs {},
   naersk ? pkgs.callPackage sources.naersk {},
-  nur ? pkgs.callPackage sources.NUR {},
 }:
-
-let
-  rust = nur.repos.mozilla.latest.rustChannels.stable.rust;
-in naersk.buildPackage {
-  name = "rnix-lsp";
-  root = nur.repos.jd91mzm2.lib.cleanSourceRust ./.;
-
-  cargo = rust.cargo;
-  rustc = rust.rustc;
-}
+naersk.buildPackage ./.
