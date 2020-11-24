a9cbcb4f2719cc11180e9d6ce2b6518d81552b9d
jD91mZM2 <me@krake.one>
2020-11-24 19:08:01 +0100

    Cleanup flake.nix further

--- flake.nix
+++ flake.nix
@@ -1,23 +1,30 @@
 {
   description = "A work-in-progress language server for Nix, with syntax checking and basic completion";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.naersk.url = "github:nmattia/naersk";
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs = {
+    naersk.url = "github:nmattia/naersk";
+    utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-
-        defaultPackage = naersk-lib.buildPackage ./.;
-
-        defaultApp = {
-          type = "app";
-          program = "${self.defaultPackage."${system}"}/bin/rnix-lsp";
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in rec {
+        packages.rnix-lsp = naersk-lib.buildPackage {
+          pname = "rnix-lsp";
+          root = ./.;
         };
+        defaultPackage = packages.rnix-lsp;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
+        };
+
+        apps.rnix-lsp = utils.lib.mkApp {
+          drv = packages.rnix-lsp;
+        };
+        defaultApp = apps.rnix-lsp;
       });
 }
