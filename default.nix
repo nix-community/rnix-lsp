984a11a5c28ec6f4ea6ff8a63ba106ec7ae22dab
Isaac van Bakel <ivb@vanbakel.io>
2020-12-04 14:17:34 +0000

    Pin flake-compat version
    This pins the version to the latest commit at the time of writing this
    message, along with a sha256 hash.

--- default.nix
+++ default.nix
@@ -1,3 +1,1 @@
-(import (builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
-  src = ./.;
-}).defaultNix.default
+(import ./flake-compat.nix).defaultNix.default
